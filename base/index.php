fe29ebed014b995e622fafb2cf9c8f4114f7b5a9
romzych <33629899+romzych@users.noreply.github.com>
2017-11-13 17:38:11 +0100

    base
    base

---
+++ base/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Portail Leonard de Vinci</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css"
+</head>
+<body>
+
+</body>
+<script type="text/javascript" src="js/bootstrap.min.js"></script></script>
+</html>
