10a96e184e86b442a2ce1b358bc659bd683e70d4
Nakaah <lucas.duchassin@devinci.fr>
2017-11-13 17:55:56 +0100

    header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="base/css/style.css">
+    <link rel="stylesheet" href="base/css/bootstrap.min.css">
+
+  </head>
+
+
+  <body>
+
+    <header>
+
+    </header>
+
+  </body>
+
+</html>
