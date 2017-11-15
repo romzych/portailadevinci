9b4f4fc78dc5d05b7767ec0b09fa1c814f69b463
romzych <33629899+romzych@users.noreply.github.com>
2017-11-15 17:39:20 +0100

    css

--- base/index.php
+++ base/index.php
@@ -4,10 +4,132 @@
     <meta charset="UTF-8">
     <title>Portail Leonard de Vinci</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css"
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <div class="col-12 bleu">
+            <p class="abs">Absences de Monsieur Roman BLONDEL</p>
+            <div class="row">
+                <div class="col-md-3">
+                    <ul class="nav flex-column" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active contour" href="#profile" role="tab" data-toggle="tab"><button type="button" class="btn btn-secondary taille2">2017 - PAIIMWB2
+                                    <br>IIM - ANNEE 2 -<br> AXE DEVELOPPEMENT WEB</button></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#buzz" role="tab" data-toggle="tab"><button type="button" class="btn btn-secondary taille">2016 - PAIIMNS1
+                                    <br>IIM - ANNEE 1 -<br> Année préparation</button></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-9">
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane in active" id="profile">
+                            <table class="table table-striped bordernaze">
+                                <thead>
+                                <span class="badge badge-info">1 Absence Semestre 1</span>
+                                <span class="badge badge-success">0 Absence Semestre 2</span>
+                                :
+                                <span class="badge badge-warning bug">1 Absence totale</span>
+                                :
+                                <span class="badge badge-info">2 séances</span>
+                                IIM - ANNEE 2 - AXE DEVELOPPEMENT WEB
+                                <tr>
+                                    <th>#</th>
+                                    <th>Cours</th>
+                                    <th>Type</th>
+                                    <th>Jour</th>
+                                    <th>Créneau</th>
+                                    <th>Durée</th>
+                                    <th>Modalité</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="table-active bordernaze">
+                                    <th scope="row">1</th>
+                                    <td><span class="badge badge-default">MIIMLV210117</span>Anglais</td>
+                                    <td>COURS LV</td>
+                                    <td>2017-10-17</td>
+                                    <td>13:00:00</td>
+                                    <td>01:30:00</td>
+                                    <td>ABSENT</td>
+                                <tr class="table-active bordernaze">
+                                    <td></td>
+                                    <td><span class="badge badge-default">MIIMLV210117</span>Créativité et innovation</td>
+                                    <td>COURS LV</td>
+                                    <td>2017-10-17</td>
+                                    <td>13:00:00</td>
+                                    <td>01:30:00</td>
+                                    <td>ABSENT</td>
+                                </tr>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="buzz">
+                            <table class="table table-striped bordernaze">
+                                <thead>
+                                <span class="badge badge-info">1 Absence Semestre 1</span>
+                                <span class="badge badge-success">0 Absence Semestre 2</span>
+                                :
+                                <span class="badge badge-warning bug">1 Absence totale</span>
+                                :
+                                <span class="badge badge-info">2 séances</span>
+                                IIM - ANNEE 1    - Année préparatoire
+                                <tr>
+                                    <th>#</th>
+                                    <th>Cours</th>
+                                    <th>Type</th>
+                                    <th>Jour</th>
+                                    <th>Créneau</th>
+                                    <th>Durée</th>
+                                    <th>Modalité</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="table-active bordernaze">
+                                    <th scope="row">1</th>
+                                    <td><span class="badge badge-default">MIIMLV210117</span>Anglais</td>
+                                    <td>COURS LV</td>
+                                    <td>2017-10-17</td>
+                                    <td>13:00:00</td>
+                                    <td>01:30:00</td>
+                                    <td>ABSENT</td>
+                                </tr>
+                                <tr class="table-active bordernaze">
+                                    <td></td>
+                                    <td><span class="badge badge-default">MIIMLV210117</span>Créativité et innovation</td>
+                                    <td>COURS LV</td>
+                                    <td>2017-10-17</td>
+                                    <td>13:00:00</td>
+                                    <td>01:30:00</td>
+                                    <td>ABSENT</td>
+                                </tr>
+                                <tr class="table-active bordernaze">
+                                    <td></td>
+                                    <td><span class="badge badge-default">MIIMLV210117</span>Créativité et innovation</td>
+                                    <td>COURS LV</td>
+                                    <td>2017-10-17</td>
+                                    <td>13:00:00</td>
+                                    <td>01:30:00</td>
+                                    <td>ABSENT</td>
+                                </tr>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+        </div>
+    </div>
+</div>
 </body>
-<script type="text/javascript" src="js/bootstrap.min.js"></script></script>
+<script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+
 </html>
